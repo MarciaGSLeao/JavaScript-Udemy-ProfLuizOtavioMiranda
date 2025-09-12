618a206f4a122f10413c2ed196cc4f5a801121bb
Marcia Gameleira <gameleira.mag@hotmail.com>
2025-09-11 21:18:17 -0300

    .

---
+++ Sec03-Logica/aula38-operadoresLogicos.js
@@ -0,0 +1,11 @@
+/*
+    && - and
+    || - or
+     ! - not
+*/
+
+const usuario = 'Marcia';
+const senha = 1234;
+
+const autorizaLog = usuario == 'Marcia' && senha == 1234;
+console.log(autorizaLog);
